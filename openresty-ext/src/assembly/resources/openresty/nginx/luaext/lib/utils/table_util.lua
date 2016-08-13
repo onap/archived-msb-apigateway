1ae0c8d11018ac804be96062c34fcf0d9015ef35
HuabingZhao <zhaohuabing@zte.com.cn>
2016-08-13 14:10:39 +0800

    Initial code import
    Change-Id: I839b84e5600aedece6c33deb16bec1bf9dbb61df
    Signed-off-by: HuabingZhao <zhao.huabing@zte.com.cn>

---
+++ openresty-ext/src/assembly/resources/openresty/nginx/luaext/lib/utils/table_util.lua
@@ -0,0 +1,30 @@
+--[[
+
+    Copyright (C) 2016 ZTE, Inc. and others. All rights reserved. (ZTE)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+--]]
+
+local _M = {}
+_M._VERSION = '1.0.0'
+
+function _M.isempty(t)
+	if t == nil or next(t) == nil then
+		return true
+	else
+		return false
+	end
+end
+
+return _M
