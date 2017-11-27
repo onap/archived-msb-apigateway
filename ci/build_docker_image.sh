e477b97cdc6b8ebd5a9c9b1ed20880352fdcecf5
Victor Morales <victor.morales@intel.com>
2017-11-27 12:55:48 -0800

    Add proxy support to build msb_base
    The process to create msb_base docker image doesn't support builds
    behind a corporate proxy. The addition of respective support can
    be benefitial for those developers who work inside of corporate
    networks.
    
    Change-Id: Id9201a7298748d2c3e1cc58b95f1516aa8b0bec0
    Signed-off-by: Victor Morales <victor.morales@intel.com>
    Issue-ID: MSB-100

--- ci/build_docker_image.sh
+++ ci/build_docker_image.sh
@@ -22,7 +22,14 @@ if [[ ${NAME} && ${VERSION} && ${DIR} ]]; then
 	echo "assign the x to all files and dirs under current dir.."
 	chmod +x -R .
 	echo "begin to build image ${NAME}.."
-	docker build --no-cache -t ${NAME}:${VERSION} . >/dev/null || { echo -e "\nBuild docker image failed!";exit 1; }
+        build_args=""
+        if [ $http_proxy ]; then
+            build_args+=" --build-arg http_proxy=$http_proxy"
+        fi
+        if [ $https_proxy ]; then
+            build_args+=" --build-arg https_proxy=$https_proxy"
+        fi
+	docker build --no-cache -t ${NAME}:${VERSION} $build_args . >/dev/null || { echo -e "\nBuild docker image failed!";exit 1; }
 	docker rmi $(docker images | grep "^<none>" | awk '{print $3}') &>/dev/null
 	# docker save -o ${NAME}.tar ${NAME}:${VERSION} >/dev/null || { rm -f ${NAME}.tar &>/dev/null;echo -e "\nSave docker image failed!";exit 1; }
 	if [ ! -d ${DIR} ]; then
